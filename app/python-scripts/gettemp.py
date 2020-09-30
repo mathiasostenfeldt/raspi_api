0bfdd92ad7df5c65092188bf461b9caf07058b64
mathias.ostenfeldt@gmail.com <mathias.ostenfeldt@gmail.com>
2020-09-30 13:14:31 +0200

    remove while

--- app/python-scripts/gettemp.py
+++ app/python-scripts/gettemp.py
@@ -10,13 +10,12 @@ sensor = 7  # The Sensor goes on digital port 4.
 blue = 1    # The Blue colored sensor.
 white = 0   # The White colored sensor.
  
-while True:
-    try:
-        # This example uses the blue colored sensor.
-        # The first parameter is the port, the second parameter is the type of sensor.
-        [temp,humidity] = grovepi.dht(sensor,white)  
-        if math.isnan(temp) == False and math.isnan(humidity) == False:
-            print(temp)
- 
-    except IOError:
-        print ("Error")
+try:
+    # This example uses the blue colored sensor.
+    # The first parameter is the port, the second parameter is the type of sensor.
+    [temp,humidity] = grovepi.dht(sensor,white)  
+    if math.isnan(temp) == False and math.isnan(humidity) == False:
+        print(temp)
+
+except IOError:
+    print ("Error")
