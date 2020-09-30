1639427d97629470d14507a8dc640e6bc21940a6
mathias.ostenfeldt@gmail.com <mathias.ostenfeldt@gmail.com>
2020-09-30 13:44:33 +0200

    python changes

---
+++ app/python-scripts/turnoffinroom.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+
+import time
+import sys
+from grovepi import *
+
+# Connect the Grove LED to digital port D4
+
+if sys.argv[1] == 2:
+    led = 2
+    if digitalRead(led) == 1:
+        pinMode(led,"OUTPUT")
+        digitalWrite(led,0)           # Send LOW to switch on LED
+
+if sys.argv[1] == 3:
+    led = 3
+    if digitalRead(led) == 1:
+        pinMode(led,"OUTPUT")
+        digitalWrite(led,0)
+
+if sys.argv[1] == 4:
+    led = 4
+    if digitalRead(led) == 1:
+        pinMode(led,"OUTPUT")
+        digitalWrite(led,0)
