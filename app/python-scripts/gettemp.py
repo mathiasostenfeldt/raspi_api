d47eadcf104e74be302932aef047d57c3322f76d
mathias.ostenfeldt@gmail.com <mathias.ostenfeldt@gmail.com>
2020-09-30 13:13:28 +0200

    change script

--- app/python-scripts/gettemp.py
+++ app/python-scripts/gettemp.py
@@ -1,11 +1,22 @@
-# home_temp_hum_display.py.py
-#
-# In this project, the Temperature and humidity from the DHT sensor is printed on the DHT sensor
-
-from grovepi import *
-
-dht_sensor_port = 7             # Connect the DHt sensor to port 7
-[ temp,hum ] = dht(dht_sensor_port,1)           #Get the temperature and Humidity from the DHT sensor
-t = str(temp)
-h = str(hum)
-print("temp: ", t)
+import grovepi
+import math
+# Connect the Grove Temperature & Humidity Sensor Pro to digital port D4
+# This example uses the blue colored sensor.
+# SIG,NC,VCC,GND
+sensor = 7  # The Sensor goes on digital port 4.
+ 
+# temp_humidity_sensor_type
+# Grove Base Kit comes with the blue sensor.
+blue = 1    # The Blue colored sensor.
+white = 0   # The White colored sensor.
+ 
+while True:
+    try:
+        # This example uses the blue colored sensor.
+        # The first parameter is the port, the second parameter is the type of sensor.
+        [temp,humidity] = grovepi.dht(sensor,white)  
+        if math.isnan(temp) == False and math.isnan(humidity) == False:
+            print(temp)
+ 
+    except IOError:
+        print ("Error")
